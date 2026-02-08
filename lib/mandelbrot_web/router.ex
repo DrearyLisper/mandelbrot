f12b6ac655a38d969aec8e6c75409537d2cd4194
Dreary Lisper <drearylisper@gmail.com>
2026-02-08 14:47:29 +0000

    Adding CSP

--- lib/mandelbrot_web/router.ex
+++ lib/mandelbrot_web/router.ex
@@ -7,7 +7,10 @@ defmodule MandelbrotWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {MandelbrotWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{
+      "content-security-policy" =>
+        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self' ws: wss:"
+    }
   end
 
   pipeline :api do
